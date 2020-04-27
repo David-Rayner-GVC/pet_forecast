2f28950b8419e8d9d914f857a257974abb514705
David-Rayner-GVC <David.Rayner@gvc.gu.se>
2020-04-27 15:44:06 +0200

    change file names to be more descriptive add full_workflow

---
+++ full_workflow.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Full workflow to update git-hosted PET forecast data.
+
+NO PET is calculated, yet! Just the met data!
+
+@author: xrayda
+"""
+
+# Workflow 1 - assemble grids on local host
+
+import icon_url_lib
+import gridded_file_lib as gf
+
+# download the latest forecast grids
+ii = icon_url_lib.icon_url_lib()
+  
+hh = ii.GetTimeOfMostRecent()
+  
+gf.Cleanout()
+gf.DownloadPETForecastData(hh)
+
+# perform preprocessing on the grids
+concatFiles = gf.IndexLocalForecastData()
+gf.PostProcessForecastData(concatFiles)
+
+# Workflow 2 - extract time-series & udpate the distributed copies on GIT
+import pet_git_integration_lib as pgi
+
+pgi.UpdateLocalForecast
+pgi.UpdatePublisehedForecasts
+
+
+
