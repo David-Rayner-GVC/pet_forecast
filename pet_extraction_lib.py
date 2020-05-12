67f16022b3839dd38ed4e61d2e081cc7cd198a92
David-Rayner-GVC <David.Rayner@gvc.gu.se>
2020-05-12 14:06:40 +0200

    Calculates PET!!

--- pet_extraction_lib.py
+++ pet_extraction_lib.py
@@ -9,13 +9,59 @@ import config
 from xarray import DataArray, Dataset
 from datetime import datetime, timedelta
 import json
-import pandas
+import pandas as pd
 import xarray as xr
 import os
 import glob
 import re
 import numpy as np
 
+# and this is a local one
+from petprocessingprognose import petcalcprognose
+
+
+def CalculatePET(xd):
+  """
+  CalculatePET - add PET and UTCI variables to a dataset!
+  This is called internally in ExtractPETForecastData but
+  it can be called stand-alone with an xd with suitable variables
+  
+  Input/output
+  xd - xarray dataset 
+  """
+  timestamp = xd.time.data  
+  lat = float(xd.lat.data) 
+  lon = float(xd.lon.data) 
+  UTC=1
+
+  year = pd.to_datetime(timestamp).year  
+  month = pd.to_datetime(timestamp).month  
+  day =  pd.to_datetime(timestamp).day  
+  hour =  pd.to_datetime(timestamp).hour  
+  minu = pd.to_datetime(timestamp).minute 
+   
+  Ta = xd.air_temperature.data
+  RH = xd.relative_humidity.data
+  radD = xd.downward_diffuse.data
+  radI = xd.downward_direct.data
+  Ws = np.sqrt((xd.eastward_wind.data)**2 + (xd.northward_wind.data)**2)
+
+  with np.errstate(invalid='ignore'):
+    radI[radI < 0.] = 0.
+    radD[radD < 0.] = 0.
+  radG = radD + radI
+
+  poi_save = petcalcprognose(Ta, RH, Ws, radG, radD, radI, year, month, day, hour, minu, lat, lon, UTC)
+
+  xd['PET'] = (('time'), np.float32(poi_save[:, 32]) )
+  xd.PET.data[0]=np.nan
+  xd.PET.attrs = {"standard_name":"PET", "long_name":"physiological equivalent temperature", "units":"C"} 
+
+  xd['UTCI'] = (('time'), np.float32(poi_save[:, 33]) )
+  xd.UTCI.data[0]=np.nan
+  xd.UTCI.attrs = {"standard_name":"UTCI", "long_name":"Universal Thermal Climate Index", "units":"C"} 
+
+  return xd
 
 def ExtractTimeSeries(filename, cvar, lat, lon):
   """
@@ -36,7 +82,7 @@ def ExtractTimeSeries(filename, cvar, lat, lon):
     xd = xd.drop_vars('height')
   except:
     pass
-  return(xd)
+  return xd
 
 def ExtractPETForecastData(lat, lon, netcdf_dir=None):
   """
@@ -49,7 +95,9 @@ def ExtractPETForecastData(lat, lon, netcdf_dir=None):
   
   The files to use are specified in config.py as PET_vars
   """
- 
+  if config.debug:
+    print('ExtractPETForecastData lat=%f, lon=%f'%(lat, lon))
+    
   if netcdf_dir==None:
     netcdf_dir = os.path.join(config.target_root,'netcdf_final')
     
@@ -65,14 +113,14 @@ def ExtractPETForecastData(lat, lon, netcdf_dir=None):
       xa = ExtractTimeSeries(filePath, cvar, lat, lon)
       xa.name=config.standard_names[fileLabel] 
       xList.append(xa)
-  
-  if config.debug:
-    print(xr)
     
   xd = xr.merge(xList)
   xd['air_temperature'].data = xd['air_temperature'].data-273.15
   xd['air_temperature'].attrs['units']='C'  
   xd['time'].attrs['time_zone']='UTC'
+  
+  # now add PET and UTCI
+  CalculatePET(xd)
   
   return xd
   
