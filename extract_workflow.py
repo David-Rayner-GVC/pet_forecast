48571ba500fc870d04036f84218be9c9ffada47a
David-Rayner-GVC <David.Rayner@gvc.gu.se>
2021-09-14 09:15:22 +0200

    update

--- extract_workflow.py
+++ extract_workflow.py
@@ -6,15 +6,14 @@ Just extrac time-series and update git-hosted PET forecast data.
 """
 import config
 
-# Workflow 2 - extract time-series & udpate the distributed copies on GIT
-import pet_git_integration_lib as pgi
 import pet_extraction_lib as pel
-
 pel.UpdateLocalForecast(stash=True)
-pgi.UpdatePublisehedForecasts()
 
 import plotting
 plotting.UpdateLocalPlots(useLocalData=True, addTime=False)
+
+import pet_git_integration_lib as pgi
+pgi.UpdatePublisehedForecasts()
 
 
 if config.debug:
